3fe59204f9c9433a1c76bc582bc1542494207657
vinod10 <vinodkumar@kwiqpick.com>
2017-10-07 22:24:47 +0530

    added html for about

--- app/assets/scripts/scripts.js
+++ app/assets/scripts/scripts.js
@@ -1,2 +1,20 @@
 
 
+;(function($) {
+    "use strict";
+
+    $(document).ready(function() {
+
+    });
+
+    $(window).on('load' ,function () {
+
+        parallaxInit();
+    });
+
+    function parallaxInit() {
+
+        $('#section-home').parallax("30%", 0.3);
+    }
+
+})(jQuery);
