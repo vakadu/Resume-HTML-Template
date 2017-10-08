711ab32d744b2a15e3e61befb389b9c90cdd3157
vinod10 <vinodkumar@kwiqpick.com>
2017-10-09 00:21:45 +0530

    added new landing page

--- app/assets/scripts/scripts.js
+++ app/assets/scripts/scripts.js
@@ -1,15 +1,19 @@
-
-
-;(function($) {
+(function($) {
     "use strict";
 
     $(document).ready(function() {
 
+        var scroll = new SmoothScroll();
+        scroll.init({
+            speed: 1000,
+            easing: 'easeOutCubic'
+        });
     });
 
     $(window).on('load' ,function () {
 
         parallaxInit();
+        initTyped();
         new WOW().init();
     });
 
@@ -19,3 +23,41 @@
     }
 
 })(jQuery);
+
+function initTyped() {
+    $("#typed").typed({
+        strings: ["A Designer", "A Freelancer", "A Developer"],
+        // typing speed
+        typeSpeed: 300,
+        // time before typing starts
+        startDelay: 100,
+        // backspacing speed
+        backSpeed: 50,
+        // time before backspacing
+        backDelay: 3000,
+        // loop
+        loop: true,
+        // false = infinite
+        loopCount: false,
+        // show cursor
+        showCursor: true,
+        // character for cursor
+        cursorChar: ".",
+        // attribute to type (null == text)
+        attr: null,
+        // either html or text
+        contentType: 'html',
+        // call when done callback function
+        callback: function() {
+        },
+        // starting callback function before each string
+        preStringTyped: function() {
+        },
+        //callback for every typed string
+        onStringTyped: function() {
+        },
+        // callback for reset
+        resetCallback: function() {
+        }
+    });
+}
